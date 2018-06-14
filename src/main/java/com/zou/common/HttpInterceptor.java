1427a056e1529a13cc4f5fbf9a413c3a9b852984
zoulvzhou <940933073@qq.com>
2018-06-14 10:54:51 +0800

    Http前后监听修改

--- src/main/java/com/zou/common/HttpInterceptor.java
+++ src/main/java/com/zou/common/HttpInterceptor.java
@@ -16,6 +16,8 @@ public class HttpInterceptor extends HandlerInterceptorAdapter {
 
     Logger logger = LoggerFactory.getLogger(HttpInterceptor.class);
 
+    private static final String START_TIME = "requestStartTime";
+
     //请求处理之前的方法
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -38,9 +40,15 @@ public class HttpInterceptor extends HandlerInterceptorAdapter {
 
         String url = request.getRequestURL().toString();
 
-        Map parameterMap = request.getParameterMap();
+//        Map parameterMap = request.getParameterMap();
+//
+//        logger.info("request finished url:{},params:{}", url, JsonMapper.object2String(parameterMap));
 
-        logger.info("request finished url:{},params:{}", url, JsonMapper.object2String(parameterMap));
+
+        long start = (Long) request.getAttribute(START_TIME);
+        long end = System.currentTimeMillis();
+        logger.info("request  finished  url:{},cost:{}", url, end - start);
+
 
     }
 
