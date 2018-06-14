f9e7dc005bdf2d95254c470814ee15ff623b3bfb
zoulvzhou <940933073@qq.com>
2018-06-14 10:40:03 +0800

    Http请求前后监听实现

---
+++ src/main/java/com/zou/common/HttpInterceptor.java
@@ -0,0 +1,88 @@
+package com.zou.common;
+
+import com.zou.util.JsonMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Slf4j
+public class HttpInterceptor extends HandlerInterceptorAdapter {
+
+    Logger logger = LoggerFactory.getLogger(HttpInterceptor.class);
+
+    //请求处理之前的方法
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+
+        String url = request.getRequestURL().toString();
+
+        Map parameterMap = request.getParameterMap();
+
+        logger.info("request start url:{},params:{}", url, JsonMapper.object2String(parameterMap));
+
+        return true;
+    }
+
+
+    // 请求正常结束的时候调用的方法
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+
+        String url = request.getRequestURL().toString();
+
+        Map parameterMap = request.getParameterMap();
+
+        logger.info("request finished url:{},params:{}", url, JsonMapper.object2String(parameterMap));
+
+    }
+
+    //任何时候请求结束调用
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+        String url = request.getRequestURL().toString();
+
+        Map parameterMap = request.getParameterMap();
+
+        logger.info("exceptiopn  url:{},params:{}", url, JsonMapper.object2String(parameterMap));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
